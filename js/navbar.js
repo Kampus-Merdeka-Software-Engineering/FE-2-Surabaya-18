d6f20bc2c454002291fa69c746d37be82655a59d
indirahanefa <149576008+indirahanefa@users.noreply.github.com>
2023-12-06 20:17:31 +0700

    Delete js/navbar.js

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-const headerEl = document.querySelector('.header');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 350) {
-        headerEl.classList.add('active');
-    } else if (window.scrollY <= 350) {
-        headerEl.classList.remove('active');
-    }
-});
