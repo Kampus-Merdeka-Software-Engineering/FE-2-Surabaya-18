4d9ed061223dfd075dcef52db77502a2bc44aa67
harahadian <harahadian@gmail.com>
2023-12-11 19:56:32 +0700

    tes

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://be-2-surabaya-18-production.up.railway.app/';
+const API_BASE_URL = 'http://localhost:3300';
 
 async function addcontact() {
     const Customer_name = document.getElementById('name').value;
